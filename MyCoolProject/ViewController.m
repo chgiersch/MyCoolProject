c046a216d800f57506bf170ea8b52652ebdaf380
Chris Giersch <chgiersch@gmail.com>
2015-01-06 15:12:20 -0800

    first commit

--- MyCoolProject/ViewController.m
+++ MyCoolProject/ViewController.m
@@ -14,6 +14,7 @@
 
 @implementation ViewController
 
+// Added comment here
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
